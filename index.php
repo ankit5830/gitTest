8a4ee435d698992b0f29a0f6a582c0dd4b630bc0
ankit5830 <ankit.4659@gmail.com>
2017-11-06 16:03:07 +1300

    add index.php file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"  />
+<title>Angular JS</title>
+<script type="text/javascript" src="js/angular.min.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+</head>
+
+<body ng-app = "firstApp">
+	<div ng-controller = "FirstController as first">
+	<h3>Welocme to {{scope.products.test}} Angular JS section {{first.products.name}}</h3>
+	</div>
+</body>
+</html>
